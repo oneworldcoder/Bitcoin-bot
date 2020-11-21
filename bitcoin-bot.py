951a045c08984fbedba67be6fcf06880b16ee499
oneworldcoder <52066039+oneworldcoder@users.noreply.github.com>
2020-11-21 14:32:53 +0530

    Update bitcoin-bot.py

--- bitcoin-bot.py
+++ bitcoin-bot.py
@@ -30,11 +30,40 @@ data = pd.read_csv(DATASET_URL)
 
 data.head()
 
-data.dropna()
+df= data.dropna()
+df
 
-X = data[['Timestamp', 'Open', 'Low', 'Close', 'Volume_(BTC)', 'Volume_(Currency)', 'Weighted_Price']].values
-y = data[['High']]
+df['High'].plot(figsize=(12,8))
+
+X = df[['Timestamp', 'Open', 'Low', 'Close', 'Volume_(BTC)', 'Volume_(Currency)', 'Weighted_Price']].values
+y = df[['High']]
+
 
 X_train, X_test, y_train, y_test =  train_test_split(X, y, test_size=0.2)
+
+
+from pmdarima import auto_arima
+import warnings
+warnings.filterwarnings("ignore")
+
+# from statsmodels.tsa.stattools import adfuller
+
+# def ad_test(df):
+#      df = adfuller(df, autolag = 'AIC')
+#      print("1. ADF : ",df[0])
+#      print("2. P-Value : ", df[1])
+#      print("3. Num Of Lags : ", df[2])
+#      print("4. Num Of Observations Used For ADF Regression:", df[3])
+#      print("5. Critical Values :")
+#      for key, val in df[4].items():
+#          print("\t",key, ": ", val)
+
+# ad_test(df['High'])
+
+fit = auto_arima(y_train, trace= True, supress_warnings= True)
+fit.summary()
+
+
+
 
 jovian.commit(project=project)
