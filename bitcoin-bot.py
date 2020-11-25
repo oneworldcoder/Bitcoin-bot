64a95b57edd4d3b24afd2958597297d11be4d103
oneworldcoder <52066039+oneworldcoder@users.noreply.github.com>
2020-11-25 14:28:41 +0530

    Update bitcoin-bot.py

--- bitcoin-bot.py
+++ bitcoin-bot.py
@@ -43,3 +43,23 @@ training_set = sc.fit_transform(training_set)
 X_train = training_set[0:len(training_set)-1]
 y_train = training_set[1:len(training_set)]
 X_train = np.reshape(X_train, (len(X_train), 1, 1))
+
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
+# Initialising the RNN
+regressor = Sequential()
+
+# Adding the input layer and the LSTM layer
+regressor.add(LSTM(units = 4, activation = 'sigmoid', input_shape = (None, 1)))
+
+# Adding the output layer
+regressor.add(Dense(units = 1))
+
+# Compiling the RNN
+regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# Fitting the RNN to the Training set
+regressor.fit(X_train, y_train, batch_size = 5, epochs = 100)
